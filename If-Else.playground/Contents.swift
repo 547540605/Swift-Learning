901a91134e7464adb32be9a0ad179799f5c251e4
Bale <sm547540605@gmail.com>
2024-06-06 19:57:18 +0800

    start to learn if-else part.

--- If-Else.playground/Contents.swift
+++ If-Else.playground/Contents.swift
@@ -1,3 +1,12 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+let myName = "Bale"
+let myAge = 21
+let yourName = "cjm"
+let yourAge = 20
+
+if myName == "Bale"{
+    "Your name is \(myName)"
+}else{
+    "I guess it wrong"
+}
