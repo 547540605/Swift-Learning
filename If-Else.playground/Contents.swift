2c8c29928d875fac6b7ce828156e151cc225221e
Bale <sm547540605@gmail.com>
2024-06-12 11:03:08 +0800

    add If-Else

--- If-Else.playground/Contents.swift
+++ If-Else.playground/Contents.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 let myName = "Bale"
-let myAge = 21
+let myAge = 27
 let yourName = "cjm"
 let yourAge = 20
 
@@ -9,4 +9,27 @@ if myName == "Bale"{
     "Your name is \(myName)"
 }else{
     "I guess it wrong"
+}
+
+
+if myAge == 27 || myName == "FOOOO"{
+    "Either age is 20, name is Foo or both"
+}else if myName == "Bale" || myAge == 27 {
+    "It's too late to get in this clause"
+}
+
+if myName == "Bale"
+    && myAge == 21
+    && yourName == "Foo"
+    || yourAge == 20{
+    "My name is Bale and I'm 27 and your name is Foo...OR...you are 20"
+}
+
+if (myName == "Bale"
+    && myAge == 21)
+    && (yourName == "Foo"
+    || yourAge == 20){
+    "My name is Bale and I'm 27 and your name is Foo...OR...you are 20"
+}else{
+    "something go wrong"
 }
